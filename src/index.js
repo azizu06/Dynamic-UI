1f899d40a5541e55ab9a6f34360038a459fa52d2
azizu06 <ab725492@ucf.edu>
2025-12-10 01:37:32 -0500

    Add self advancing slide with time interval

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './styles.css';
 import { drop } from './dropdown.js';
-import { circleNav, slider } from './carousel.js';
+import { circleNav, slider, slideRight } from './carousel.js';
 
 const dropdown = document.querySelector('.menu');
 const items = document.querySelector('.items');
 
 drop(dropdown, items);
-slider();
 circleNav();
+slider();
+window.setInterval(slideRight, 5000);
