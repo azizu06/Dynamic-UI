00aaa5e010cec742b57fbb6177b360a5c862cba9
azizu06 <ab725492@ucf.edu>
2025-12-10 00:53:08 -0500

    Load setup functions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './styles.css';
 import { drop } from './dropdown.js';
+import { circleNav, slider } from './carousel.js';
 
 const dropdown = document.querySelector('.menu');
 const items = document.querySelector('.items');
 
 drop(dropdown, items);
+slider();
+circleNav();
